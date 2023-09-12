5db8a99db5c622e6576c2b574a55aa9b5a8f537f
Fajri1412 <144096894+Fajri1412@users.noreply.github.com>
2023-09-12 12:23:23 +0700

    First Mini Project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-function Hitung() {
+function Luas() {
     let Sisi = document.getElementById("Sisi").value;
     Sisi = parseInt(Sisi);
     let Luas = Sisi * Sisi
     document.getElementById("Luas").value = Luas;
+}
+
+function Keliling() {
+    let Sisi = document.getElementById("Sisi").value;
+    Sisi = parseInt(Sisi);
+    let Keliling = 4 * Sisi
+    document.getElementById("Keliling").value = Keliling;
+}
+
+function Reset() {
+    let Sisi = document.getElementById("Sisi").value;
+    Sisi = parseInt(Sisi);
+    let Reset= 0 * Sisi
+    document.getElementById("Luas").value = Luas;
+    document.getElementById("Keliling").value = Keliling;
 }
